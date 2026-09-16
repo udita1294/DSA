88a3d1fe8255364731c6bffcb1235674c76b38bf
UDITA SINGH <udita1294@gmail.com>
2026-09-16 12:21:48 +0530

    Time: 43 ms (58.88%), Space: 34.6 MB (39.25%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,35 +1,24 @@
-#define ll long long
 class Solution {
 public:
-    vector<ll> fact;
-    vector<ll> invFact;
-    const int MOD = 1e9+7;
-    ll findPower(ll a, ll b) {
-        if(b == 0)
-            return 1;
-        ll half = findPower(a, b/2);
-        ll result = (half * half) % MOD;
-
-        if(b%2 == 1) {
-            result = (result * a) % MOD;
+    int M = 1e9 + 7;
+    int dp[1001][1001];
+    int numberOfSets(int n, int K) {
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = (i < n) ? 1 : 0;
         }
-        return result;
-    }
-    ll nCr(int n, int r) {
-        return (((fact[n] * invFact[r]) % MOD) * invFact[n-r]) % MOD;
-    }
-
-    int numberOfSets(int n, int k) {
-        fact.assign(n+k, 1);
-        invFact.assign(n+k, 1);
-
-        for(int i = 2; i < n+k; i++) {
-            fact[i] = (fact[i-1] * i) % MOD;
+        for (int k = 1; k <= K; k++) {
+            vector<int> prevRowSum(n + 1, 0);
+            //prevRowSum[x] = dp[k-1][x] + dp[k-1][x+1] + ... + dp[k-1][n-1]
+            for (int x = n - 1; x >= 0; x--) {
+                prevRowSum[x] = (prevRowSum[x + 1] + dp[k - 1][x]) % M;
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                int skip = dp[k][i + 1];
+                int take = prevRowSum[i+1];
+                //dp[k-1][i+1] + dp[k-1][i+2] ...... + dp[k-1][n-1]
+                dp[k][i] = (take + skip) % M;
+            }
         }
-        for(int i = 0; i < n+k; i++) {
-            invFact[i] = findPower(fact[i], MOD-2);
-        }
-
-        return nCr(n+k-1, 2*k);
+        return dp[K][0];
     }
 };
